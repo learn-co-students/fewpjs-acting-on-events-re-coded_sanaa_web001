f7a31abc3f7c7967a88d4e5bafa75e52f20355b0
Ahmed Aleryani <aalavyani91@yahoo.com>
2020-07-25 23:21:09 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Your code here
+
+var dodger = document.getElementById('dodger');
+
+
+function moveDodgerLeft() {
+    var leftNumbers = dodger.style.left.replace('px', '');
+    var left = parseInt(leftNumbers, 10)
+    if (left > 0) {
+        dodger.style.left = `${left - 2}px`
+    }
+}
+
+
+function moveDodgerRight() {
+    var leftNumbers = dodger.style.left.replace('px', '');
+    var left = parseInt(leftNumbers, 10)
+    if (left > 0) {
+        dodger.style.left = `${left + 2}px`
+    }
+}
+function reset(){
+    dodger.style.left="180px";
+
+}
+
+document.addEventListener('keydown', function (e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft()
+    }
+})
+
+
+document.addEventListener('keydown', function (e) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight()
+    }
+})
+
+document.addEventListener('keydown', function (e) {
+    if (e.keyCode===32) {
+        reset();
+    }
+})
