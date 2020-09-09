6a8642c5753957dbd103a535e05f509271a2178a
Fatima Amin <fatima.amin4official@gmail.com>
2020-09-09 07:17:56 +0300

    adding

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+// Your code here
+let dodger = document.getElementById('dodger') ;
+
+function moveDodgerLeft() {
+  var leftNumber = dodger.style.left.replace('px' , '') ;
+  
+  var left = parseInt(leftNumber , 10) ;
+  if(left > 0) 
+    dodger.style.left = `${left - d1}px` ;
+  
+  
+}
+
+function moveDodgerRight() {
+  var leftNumber = dodger.style.left.replace('px' , '') ;
+  
+  var left = parseInt(leftNumber , 10) ;
+  if(left > 0) 
+    dodger.style.left = `${left + 1}px` ;
+  
+  
+}
+
+document.addEventListener('keydown' , e => {
+  if(e.key === 'ArrowLeft') {
+    moveDodgerLeft();
+  }
+ 
+})
+
+
+document.addEventListener('keydown' , e => {
+  if(e.key === 'ArrowRight') {
+    moveDodgerRight();
+  }
+ 
+})
